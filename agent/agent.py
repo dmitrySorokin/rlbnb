1ecc99ae16d489c4e05632a22001b920b346ab50
Ivan Nazarov <ivannz@yandex.ru>
2022-12-11 17:39:06 +0100

    moved weight init logic out of the methd

--- agent/agent.py
+++ agent/agent.py
@@ -1,8 +1,107 @@
 import torch
+from torch import nn
 import torch_geometric
 import numpy as np
-from torch_geometric.data import Batch
+from torch_geometric.data import Batch  # noqa: F401
+
 import ecole
+
+from typing import NamedTuple
+from ecole.observation import NodeBipartiteObs
+from ecole.observation import coo_matrix as EdgeFeatures
+from torch import Tensor, as_tensor
+
+
+class EdgeObs(NamedTuple):
+    inx: Tensor
+    val: Tensor
+
+    @classmethod
+    def from_ecole(cls, obs: EdgeFeatures, device: torch.device = None) -> ...:
+        return cls(
+            inx=as_tensor(obs.indices.astype(int), dtype=torch.long, device=device),
+            val=as_tensor(obs.values, dtype=torch.float32, device=device).view(-1, 1),
+        )
+
+
+class BipartiteObs(NamedTuple):
+    vars: Tensor
+    cons: Tensor
+    ctov: EdgeObs
+
+    @classmethod
+    def from_ecole(cls, obs: NodeBipartiteObs, device: torch.device = None) -> ...:
+        return cls(
+            vars=as_tensor(obs.variable_features, dtype=torch.float32, device=device),
+            cons=as_tensor(obs.row_features, dtype=torch.float32, device=device),
+            ctov=EdgeObs.from_ecole(obs.edge_features, device),
+        )
+
+
+def init_linear(
+    m: nn.Module, weight: str = None, nonlinearity: str = None, bias: str = None
+) -> None:
+    if not isinstance(m, nn.Linear):
+        return
+
+    if weight == "uniform":
+        nn.init.uniform_(m.weight, a=0.0, b=1.0)
+
+    elif weight == "normal":
+        nn.init.normal_(m.weight, mean=0.0, std=0.01)
+
+    elif weight == "xavier_uniform":
+        gain = nn.init.calculate_gain(nonlinearity)
+        nn.init.xavier_uniform_(m.weight, gain=gain)
+
+    elif weight == "xavier_normal":
+        gain = nn.init.calculate_gain(nonlinearity)
+        nn.init.xavier_normal_(m.weight, gain=gain)
+
+    elif weight == "kaiming_uniform":
+        nn.init.kaiming_uniform_(m.weight, nonlinearity=nonlinearity)
+
+    elif weight == "kaiming_normal":
+        nn.init.kaiming_normal_(m.weight, nonlinearity=nonlinearity)
+        # nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
+
+    elif weight is not None:
+        raise NotImplementedError(f"Linear weight `{weight}`")
+
+    if getattr(m, "bias", None) is None:
+        return
+
+    if bias == "zeros":
+        nn.init.zeros_(m.bias)
+
+    elif bias == "uniform":
+        nn.init.uniform_(m.bias)
+
+    elif bias == "normal":
+        nn.init.normal_(m.bias)
+
+    elif bias is not None:
+        raise NotImplementedError(f"Linear bias `{bias}`")
+
+
+def init_layernorm(m: nn.Module, weight: str = None, bias: str = None) -> None:
+    if not isinstance(m, nn.LayerNorm):
+        return
+
+    if weight == "normal":
+        nn.init.normal_(m.weight, mean=0.0, std=0.01)
+
+    elif weight is not None:
+        raise NotImplementedError(f"Layernorm weight `{weight}`")
+
+    if bias == "zeros":
+        nn.init.zeros_(m.bias)
+
+    elif bias == "normal":
+        nn.init.normal_(m.bias)
+
+    elif bias is not None:
+        raise NotImplementedError(f"Layernorm bias `{bias}`")
 
 
 class BipartiteGraphConvolution(torch_geometric.nn.MessagePassing):
@@ -11,31 +110,27 @@ class BipartiteGraphConvolution(torch_geometric.nn.MessagePassing):
     to provide the exact form of the messages being passed.
     """
 
-    def __init__(self, aggregator='mean', emb_size=64):
+    def __init__(self, aggregator="mean", emb_size=64):
         super().__init__(aggregator)
 
-        self.feature_module_left = torch.nn.Sequential(
-            torch.nn.Linear(emb_size, emb_size)
+        self.feature_module_left = nn.Sequential(nn.Linear(emb_size, emb_size))
+        self.feature_module_right = nn.Sequential(
+            nn.Linear(emb_size, emb_size, bias=False)
         )
-        self.feature_module_right = torch.nn.Sequential(
-            torch.nn.Linear(emb_size, emb_size, bias=False)
+        self.feature_module_final = nn.Sequential(
+            nn.LayerNorm(emb_size),
+            nn.LeakyReLU(),
+            nn.Linear(emb_size, emb_size),
         )
-        self.feature_module_final = torch.nn.Sequential(
-            torch.nn.LayerNorm(emb_size),
-            torch.nn.LeakyReLU(),
-            torch.nn.Linear(emb_size, emb_size)
-        )
-        self.post_conv_module = torch.nn.Sequential(
-            torch.nn.LayerNorm(emb_size)
-        )
+        self.post_conv_module = nn.Sequential(nn.LayerNorm(emb_size))
 
         # output_layers
-        self.output_module = torch.nn.Sequential(
-            torch.nn.Linear(2 * emb_size, emb_size),
-            # torch.nn.LayerNorm(emb_size, emb_size), # added
-            torch.nn.LeakyReLU(),
-            torch.nn.Linear(emb_size, emb_size),
-            # torch.nn.LayerNorm(emb_size, emb_size), # added
+        self.output_module = nn.Sequential(
+            nn.Linear(2 * emb_size, emb_size),
+            # nn.LayerNorm(emb_size, emb_size), # added
+            nn.LeakyReLU(),
+            nn.Linear(emb_size, emb_size),
+            # nn.LayerNorm(emb_size, emb_size), # added
         )
 
     def forward(self, left_features, edge_indices, edge_features, right_features):
@@ -47,11 +142,18 @@ class BipartiteGraphConvolution(torch_geometric.nn.MessagePassing):
         # node_features=(left_features, right_features), edge_features=edge_features)
         # output = self.propagate(edge_indices, size=(left_features.shape[0], right_features.shape[0]),
         # node_features=(self.feature_module_left(left_features), self.feature_module_right(right_features)))
-        output = self.propagate(edge_indices, size=(left_features.shape[0], right_features.shape[0]),
-                                node_features=(
-                                self.feature_module_left(left_features), self.feature_module_right(right_features)),
-                                edge_features=None)
-        return self.output_module(torch.cat([self.post_conv_module(output), right_features], dim=-1))
+        output = self.propagate(
+            edge_indices,
+            size=(left_features.shape[0], right_features.shape[0]),
+            node_features=(
+                self.feature_module_left(left_features),
+                self.feature_module_right(right_features),
+            ),
+            edge_features=None,
+        )
+        return self.output_module(
+            torch.cat([self.post_conv_module(output), right_features], dim=-1)
+        )
 
     def message(self, node_features_i, node_features_j, edge_features=None):
         # def message(self, node_features_i, node_features_j):
@@ -60,25 +162,29 @@ class BipartiteGraphConvolution(torch_geometric.nn.MessagePassing):
         # + self.feature_module_right(node_features_j))
         # output = self.feature_module_final(node_features_i + node_features_j)
         if edge_features is not None:
-            output = self.feature_module_final(node_features_i + node_features_j + edge_features)
+            output = self.feature_module_final(
+                node_features_i + node_features_j + edge_features
+            )
         else:
             output = self.feature_module_final(node_features_i + node_features_j)
         return output
 
 
-class BipartiteGCN(torch.nn.Module):
-    def __init__(self,
-                 device,
-                 emb_size=64,
-                 cons_nfeats=5,
-                 edge_nfeats=1,
-                 var_nfeats=43,
-                 num_heads=1,
-                 head_depth=1,
-                 linear_weight_init=None,
-                 linear_bias_init=None,
-                 layernorm_weight_init=None,
-                 layernorm_bias_init=None):
+class BipartiteGCN(nn.Module):
+    def __init__(
+        self,
+        device,
+        emb_size: int = 64,
+        cons_nfeats: int = 5,
+        edge_nfeats: int = 1,
+        var_nfeats: int = 43,
+        num_heads: int = 1,
+        head_depth: int = 1,
+        linear_weight_init: str = None,
+        linear_bias_init: str = None,
+        layernorm_weight_init: str = None,
+        layernorm_bias_init: str = None,
+    ) -> None:
         super().__init__()
 
         self.device = device
@@ -89,104 +195,61 @@ class BipartiteGCN(torch.nn.Module):
         self.activation = None
         self.num_heads = num_heads
         self.head_depth = head_depth
+
         self.linear_weight_init = linear_weight_init
         self.linear_bias_init = linear_bias_init
         self.layernorm_weight_init = layernorm_weight_init
         self.layernorm_bias_init = layernorm_bias_init
 
         # CONSTRAINT EMBEDDING
-        self.cons_embedding = torch.nn.Sequential(
-            torch.nn.LayerNorm(cons_nfeats),
-            torch.nn.Linear(cons_nfeats, emb_size),
-            # torch.nn.LayerNorm(emb_size, emb_size), # added
-            torch.nn.LeakyReLU(),
-            torch.nn.Linear(emb_size, emb_size),
-            # torch.nn.LayerNorm(emb_size, emb_size), # added
-            torch.nn.LeakyReLU(),
+        self.cons_embedding = nn.Sequential(
+            nn.LayerNorm(cons_nfeats),
+            nn.Linear(cons_nfeats, emb_size),
+            # nn.LayerNorm(emb_size, emb_size), # added
+            nn.LeakyReLU(),
+            nn.Linear(emb_size, emb_size),
+            # nn.LayerNorm(emb_size, emb_size), # added
+            nn.LeakyReLU(),
         )
 
         # VARIABLE EMBEDDING
-        self.var_embedding = torch.nn.Sequential(
-            torch.nn.LayerNorm(var_nfeats),
-            torch.nn.Linear(var_nfeats, emb_size),
-            # torch.nn.LayerNorm(emb_size, emb_size), # added
-            torch.nn.LeakyReLU(),
-            torch.nn.Linear(emb_size, emb_size),
-            # torch.nn.LayerNorm(emb_size, emb_size), # added
-            torch.nn.LeakyReLU(),
+        self.var_embedding = nn.Sequential(
+            nn.LayerNorm(var_nfeats),
+            nn.Linear(var_nfeats, emb_size),
+            # nn.LayerNorm(emb_size, emb_size), # added
+            nn.LeakyReLU(),
+            nn.Linear(emb_size, emb_size),
+            # nn.LayerNorm(emb_size, emb_size), # added
+            nn.LeakyReLU(),
         )
 
-        self.conv_v_to_c = BipartiteGraphConvolution(emb_size=emb_size, aggregator='mean')
-        self.conv_c_to_v = BipartiteGraphConvolution(emb_size=emb_size, aggregator='mean')
+        self.conv_v_to_c = BipartiteGraphConvolution(
+            emb_size=emb_size, aggregator="mean"
+        )
+        self.conv_c_to_v = BipartiteGraphConvolution(
+            emb_size=emb_size, aggregator="mean"
+        )
 
         heads = []
         for _ in range(self.num_heads):
             head = []
             for _ in range(self.head_depth):
-                head.append(torch.nn.Linear(emb_size, emb_size))
-                head.append(torch.nn.LeakyReLU())
-            head.append(torch.nn.Linear(emb_size, 1, bias=True))
-            heads.append(torch.nn.Sequential(*head))
-        self.heads_module = torch.nn.ModuleList(heads)
+                head.append(nn.Linear(emb_size, emb_size))
+                head.append(nn.LeakyReLU())
+            head.append(nn.Linear(emb_size, 1, bias=True))
+            heads.append(nn.Sequential(*head))
+        self.heads_module = nn.ModuleList(heads)
 
         self.init_model_parameters()
         self.to(device)
 
-
     def init_model_parameters(self, init_gnn_params=True, init_heads_params=True):
-
         def init_params(m):
-            if isinstance(m, torch.nn.Linear):
-                # weights
-                if self.linear_weight_init is None:
-                    pass
-                elif self.linear_weight_init == 'uniform':
-                    torch.nn.init.uniform_(m.weight, a=0.0, b=1.0)
-                elif self.linear_weight_init == 'normal':
-                    torch.nn.init.normal_(m.weight, mean=0.0, std=0.01)
-                elif self.linear_weight_init == 'xavier_uniform':
-                    torch.nn.init.xavier_uniform_(m.weight, gain=torch.nn.init.calculate_gain(self.activation))
-                elif self.linear_weight_init == 'xavier_normal':
-                    torch.nn.init.xavier_normal_(m.weight, gain=torch.nn.init.calculate_gain(self.activation))
-                elif self.linear_weight_init == 'kaiming_uniform':
-                    torch.nn.init.kaiming_uniform_(m.weight, nonlinearity=self.activation)
-                elif self.linear_weight_init == 'kaiming_normal':
-                    torch.nn.init.kaiming_normal_(m.weight, nonlinearity=self.activation)
-                    # torch.nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
-                else:
-                    raise Exception(f'Unrecognised linear_weight_init {self.linear_weight_init}')
+            init_linear(
+                m, self.linear_weight_init, self.activation, self.linear_bias_init
+            )
 
-                # biases
-                if m.bias is not None:
-                    if self.linear_bias_init is None:
-                        pass
-                    elif self.linear_bias_init == 'zeros':
-                        torch.nn.init.zeros_(m.bias)
-                    elif self.linear_bias_init == 'uniform':
-                        torch.nn.init.uniform_(m.bias)
-                    elif self.linear_bias_init == 'normal':
-                        torch.nn.init.normal_(m.bias)
-                    else:
-                        raise Exception(f'Unrecognised bias initialisation {self.linear_bias_init}')
-
-            elif isinstance(m, torch.nn.LayerNorm):
-                # weights
-                if self.layernorm_weight_init is None:
-                    pass
-                elif self.layernorm_weight_init == 'normal':
-                    torch.nn.init.normal_(m.weight, mean=0.0, std=0.01)
-                else:
-                    raise Exception(f'Unrecognised layernorm_weight_init {self.layernorm_weight_init}')
-
-                # biases
-                if self.layernorm_bias_init is None:
-                    pass
-                elif self.layernorm_bias_init == 'zeros':
-                    torch.nn.init.zeros_(m.bias)
-                elif self.layernorm_bias_init == 'normal':
-                    torch.nn.init.normal_(m.bias)
-                else:
-                    raise Exception(f'Unrecognised layernorm_bias_init {self.layernorm_bias_init}')
+            init_layernorm(m, self.layernorm_weight_init, self.layernorm_bias_init)
 
         if init_gnn_params:
             # init base GNN params
@@ -197,54 +260,38 @@ class BipartiteGCN(torch.nn.Module):
             for h in self.heads_module:
                 h.apply(init_params)
 
-    def forward(self, obs):
-        constraint_features = torch.from_numpy(
-            obs.row_features.astype(np.float32)
-        ).to(self.device)
-
-        edge_indices = torch.LongTensor(
-            obs.edge_features.indices.astype(np.int16)
-        ).to(self.device)
-
-        edge_features = torch.from_numpy(
-            obs.edge_features.values.astype(np.float32)
-        ).view(-1, 1).to(self.device)
-
-        variable_features = torch.from_numpy(
-            obs.variable_features.astype(np.float32)
-        ).to(self.device)
-
-        # print(constraint_features.shape, edge_indices.shape, edge_features.shape, variable_features.shape)
-        reversed_edge_indices = torch.stack([edge_indices[1], edge_indices[0]], dim=0)
+    def forward(self, obs: NodeBipartiteObs) -> Tensor:
+        obs = BipartiteObs.from_ecole(obs, self.device)
+        assert obs.vars.shape[1] == self.var_nfeats
 
         # First step: linear embedding layers to a common dimension (64)
-        constraint_features = self.cons_embedding(constraint_features)
-        assert variable_features.shape[1] == self.var_nfeats
-        variable_features = self.var_embedding(variable_features)
+        cons = self.cons_embedding(obs.cons)
+        vars = self.var_embedding(obs.vars)
 
         # Two half convolutions (message passing round)
-        constraint_features = self.conv_v_to_c(
-            variable_features, reversed_edge_indices, edge_features, constraint_features
-        )
-        variable_features = self.conv_c_to_v(
-            constraint_features, edge_indices, edge_features, variable_features
-        )
+        obs_vtoc_inx = torch.stack([obs.ctov.inx[1], obs.ctov.inx[0]], dim=0)
+        cons = self.conv_v_to_c(vars, obs_vtoc_inx, obs.ctov.val, cons)
+        vars = self.conv_c_to_v(cons, obs.ctov.inx, obs.ctov.val, vars)
 
         # get output for each head
-        head_output = [self.heads_module[head](variable_features).squeeze(-1) for head in range(self.num_heads)]
-        head_output = torch.stack(head_output, dim=0).mean(dim=0)
+        head_output = torch.stack(
+            [head(vars).squeeze(-1) for head in self.heads_module], dim=0
+        )
 
-        return head_output
+        return head_output.mean(dim=0)
 
 
 class DQNAgent:
-    def __init__(self, device):
+    def __init__(self, device, epsilon: float = None):
         self.net = BipartiteGCN(device=device, var_nfeats=24)
         self.opt = torch.optim.Adam(self.net.parameters())
+        self.epsilon = epsilon
 
-    def act(self, obs, action_set, epsilon):
+    def act(self, obs, action_set, epsilon: float = None):
+        epsilon = float(self.epsilon if epsilon is None else epsilon)
+
         with torch.no_grad():
-            preds = self.net(obs)[action_set.astype('int32')]
+            preds = self.net(obs)[action_set.astype("int32")]
 
         # single observation
         if np.random.rand() < epsilon:
@@ -270,12 +317,10 @@ class DQNAgent:
         return loss.detach().cpu().item()
 
     def save(self, path, epoch_id):
-        torch.save(self.net.state_dict(), path + f'/checkpoint_{epoch_id}.pkl')
+        torch.save(self.net.state_dict(), path + f"/checkpoint_{epoch_id}.pkl")
 
     def load(self, path):
-        self.net.load_state_dict(
-            torch.load(path, map_location=self.net.device)
-        )
+        self.net.load_state_dict(torch.load(path, map_location=self.net.device))
 
     def train(self):
         self.net.train()
@@ -285,15 +330,15 @@ class DQNAgent:
 
 
 class StrongAgent:
-    def __init__(self, env):
-        self.strong_branching_function = ecole.observation.StrongBranchingScores()
+    def __init__(self, env, epsilon: float = None):
+        self.sbf = ecole.observation.StrongBranchingScores()
         self.env = env
 
     def before_reset(self, model):
-        self.strong_branching_function.before_reset(model)
+        self.sbf.before_reset(model)
 
-    def act(self, obs, action_set, epsilon):
-        scores = self.strong_branching_function.extract(self.env.model, False)[action_set]
+    def act(self, obs, action_set, epsilon: float = None):
+        scores = self.sbf.extract(self.env.model, False)[action_set]
         return action_set[np.argmax(scores)]
 
     def eval(self):
