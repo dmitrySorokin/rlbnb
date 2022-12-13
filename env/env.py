52e5d2c88507d8e63e922fa4ed6840a93f0528d5
Dmitry Sorokin <dmitrii.sorokin@phystech.edu>
2022-12-13 21:22:20 +0700

    change env params

--- env/env.py
+++ env/env.py
@@ -22,9 +22,10 @@ class EcoleBranching(ecole.environment.Branching):
         gasse_2019_scip_params = {
             'separating/maxrounds': 0,  # separate (cut) only at root node
             'presolving/maxrestarts': 0,  # disable solver restarts
-            'limits/time': 20 * 60,  # solver time limit
-            'limits/gap': 3e-4,  # 0.03% relative primal-dual gap (default: 0.0)
-            'limits/nodes': -1,
+            'limits/time': 60 * 60,  # solver time limit
+            'timing/clocktype': 1,  # 1: CPU user seconds, 2: wall clock time
+            # 'limits/gap': 3e-4,  # 0.03% relative primal-dual gap (default: 0.0)
+            # 'limits/nodes': -1,
         }
 
         super(EcoleBranching, self).__init__(
