cc99300e73ef949505ad587e7b841e2ef6365fa5
al <Sasha252VNS@gmail.com>
2022-12-20 14:16:11 +0300

    correct scip params for imitation learning data generating

--- gen_imitation_data_tripartite.py
+++ gen_imitation_data_tripartite.py
@@ -17,12 +17,14 @@ from omegaconf import DictConfig, OmegaConf
 hydra.HYDRA_FULL_ERROR = 1
 
 n_parallel_process = 16
-scip_params = {'separating/maxrounds': 0,  # separate (cut) only at root node
-               'presolving/maxrestarts': 0,  # disable solver restarts
-               'limits/time': 20*60,  # solver time limit
-               'timing/clocktype': 2,
-               'limits/gap': 3e-4,
-               'limits/nodes': -1}
+scip_params = {
+    'separating/maxrounds': 0,  # separate (cut) only at root node
+    'presolving/maxrestarts': 0,  # disable solver restarts
+    'limits/time': 60 * 60,  # solver time limit
+    'timing/clocktype': 1,  # 1: CPU user seconds, 2: wall clock time
+    # 'limits/gap': 3e-4,  # 0.03% relative primal-dual gap (default: 0.0)
+    # 'limits/nodes': -1,
+}
 
 def run_sampler(path, sample_n_queue, co_class, co_class_kwargs, branching, max_steps=None, seed=0):
     '''
