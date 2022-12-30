5ce65f11c90ca71994c4deefbc3dcd4a2632fe2b
al <Sasha252VNS@gmail.com>
2022-12-30 13:55:54 +0300

    random/strong agents removed

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,5 +1,3 @@
 from .dqn_agent import DQNAgent
 from .imitation_agent import ImitationAgent
-from .random_agent import RandomAgent
-from .strong_agent import StrongAgent
 from .replay_buffer import ReplayBuffer
